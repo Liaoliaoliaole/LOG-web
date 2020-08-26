e70dcb3351344b6bfc988ae5979a55255e184005
Sam Harry Tzavaras <fantomsam@hotmail.com>
2020-08-27 01:37:26 +0300

    Update, compression done

--- Morfeas_WEB/morfeas_php/Supplementary.php
+++ Morfeas_WEB/morfeas_php/Supplementary.php
@@ -16,74 +16,42 @@ Copyright (C) 12019-12020  Sam harry Tzavaras
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-
-/*
-//Compression function
-function uncompress(data)
-{
-	"use strict";
-	var i,
-		dictOffset = 0,
-		dictionary = [],
-		result = "";
-
-	dictOffset = data.charCodeAt(0);
-
-	for (i=1; i<data.length; i++)
-	{
-		if(data.charAt(i)<String.fromCharCode(dictOffset))
-		{
-			dictionary.push(data.charAt(i));
-			result += data.charAt(i);
-		}
-		else
-		{
-			result += dictionary[data.charCodeAt(i)-dictOffset] + data.charAt(i+1);
-			dictionary.push(dictionary[data.charCodeAt(i)-dictOffset]+data.charAt(i+1));
-			i++;
-
-		}
-	}
-	return result;
-}
-*/
-
-function ustrtoarray($inp)
-{
-	function charSizeAt($inp, $pos)
-	{
-		if(ord($inp[$pos])<=0x7F)//Check for ASCII
-			return 1;
-		else//Char is Unicode
-		{
-			$s=ord($inp[$pos]);
-			$i=1;
-			while(1)
-			{
-				if(!(($s<<=1)&0x80))
-					break;
-				$i++;
-			}
-		}
-		return $i;
-	};
-
-	$ret_arr = array();
-	for($i=0, $c=0; $i<strlen($inp); $i+=$c)
-	{
-		$c=charSizeAt($inp, $i);
-		array_push($ret_arr, substr($inp, $i, $c));
-	}
-	return $ret_arr;
-}
-
+//Decompression function
 function decompress($data)
 {
-	mb_check_encoding($data, "UTF-8") or die("Decompression Error!!!!");
+	function unicodetoarray($inp)
+	{
+		function charSizeAt($inp, $pos)
+		{
+			if(ord($inp[$pos])<=0x7F)//Check for ASCII
+				return 1;
+			else//Char is Unicode
+			{
+				$s=ord($inp[$pos]);
+				$i=1;
+				while(1)
+				{
+					if(!(($s<<=1)&0x80))
+						break;
+					$i++;
+				}
+			}
+			return $i;
+		};
+
+		$ret_arr = array();
+		for($i=0, $c=0; $i<strlen($inp); $i+=$c)
+		{
+			$c=charSizeAt($inp, $i);
+			array_push($ret_arr, substr($inp, $i, $c));
+		}
+		return $ret_arr;
+	};
+
+	//mb_check_encoding($data, "UTF-8") or die("Decompression Error!!!!");
 	$result = "";
 	$dictionary = array();
-	$data = ustrtoarray($data);
-
+	$data = unicodetoarray($data);
 	$dictOffset = mb_ord($data[0]);
 
 	for($i=1; $i<count($data); $i++)
