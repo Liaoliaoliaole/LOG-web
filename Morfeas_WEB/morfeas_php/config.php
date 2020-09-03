10d4572b67e4d8f6fe202fe0238cc29c8c7b5f4c
fantomsam <fantomsam@hotmail.com>
2020-09-03 17:31:58 +0300

    Update

--- Morfeas_WEB/morfeas_php/config.php
+++ Morfeas_WEB/morfeas_php/config.php
@@ -1,4 +1,21 @@
 <?php
+/*
+File: config.php PHP Script for Configuration of Morfeas_system and network.
+Copyright (C) 12019-12020  Sam harry Tzavaras
+
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 	require("../Morfeas_env.php");
 	require("./Supplementary.php");
 	class eth_if_config
