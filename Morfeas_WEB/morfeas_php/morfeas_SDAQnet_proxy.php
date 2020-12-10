6781f72cc2d07bce22fc3e42ad305ea8c7ab60f5
fantomsam <fantomsam@hotmail.com>
2020-12-10 10:34:45 +0200

    Update

---
+++ Morfeas_WEB/morfeas_php/morfeas_SDAQnet_proxy.php
@@ -0,0 +1,53 @@
+<?php
+/*
+File: morfeas_SDAQnet_proxy.php PHP Script for the Morfeas_Web. Part of Morfeas_project.
+Copyright (C) 12019-12020  Sam harry Tzavaras
+
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+require("./Supplementary.php");
+ob_start("ob_gzhandler");//Enable gzip buffering
+//Disable caching
+header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Pragma: no-cache');
+header('Expires: 0');
+
+$requestType = $_SERVER['REQUEST_METHOD'];
+if($requestType == "GET")
+{
+	if(array_key_exists("SDAQnet", $_GET)&&array_key_exists("SDAQaddr", $_GET))
+	{
+		$SDAQ_net=$_GET['SDAQnet'];
+		$SDAQ_addr=$_GET['SDAQaddr'];
+		exec("SDAQ_worker $SDAQ_net getinfo $SDAQ_addr -s 2>&1", $output, $retval);
+		if(!$retval)
+		{
+			header('Content-Type: Morfeas_SDAQ_calibration_data/xml');
+			echo implode("\n",$output);
+		}
+		else
+		{
+			header('Content-Type: report/text');
+			echo "Error: $output[0]";
+		}
+		return;
+	}
+}
+else if($requestType == "POST")
+{
+	$RX_data = file_get_contents('php://input');
+	$SDAQ_cal_data = decompress($RX_data) or die("Error: Decompressing of SDAQ\'s Calibration XML data");
+}
+http_response_code(404);
+?>
