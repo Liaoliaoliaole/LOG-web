b3f201f410a9feeba6c149ef98e43b7814196098
Sam Harry Tzavaras <fantomsam@hotmail.com>
2021-05-14 18:47:09 +0300

    Update

--- Morfeas_WEB/morfeas_ecma/common.js
+++ Morfeas_WEB/morfeas_ecma/common.js
@@ -50,7 +50,7 @@ function PopupCenter(url, title, w, h)
 	var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
 	var left = ((width / 2) - (w / 2)) + dualScreenLeft;
 	var top = ((height / 2) - (h / 2)) + dualScreenTop;
-	var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+	var newWindow = window.open(url, title, "directories=no,titlebar=no,toolbar=no,location=no,status=yes,menubar=no,scrollbars=yes,width="+w+',height='+h+',top='+top+',left='+left);
 	// Puts focus on the newWindow
 	if(window.focus)
 		newWindow.focus();
