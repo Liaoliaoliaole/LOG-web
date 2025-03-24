c980ee655d7049327a1db7f69a47abd4853a9198
Lili <tenderlili@gmail.com>
2025-03-24 11:38:00 +0200

    Update

--- Morfeas_WEB/index.php
+++ Morfeas_WEB/index.php
@@ -404,7 +404,7 @@ function checkUpdateStatus() {
     })
     .then(res => res.json())
     .then(data => {
-		console.log("Update flag status" , data.update_needed);
+		console.log("Auto Check Result - update_needed:", data.update_needed);
         showUpdateIndicator(data.update_needed);
     })
     .catch(err => console.warn("Error checking update status:", err));
@@ -413,7 +413,7 @@ function checkUpdateStatus() {
 document.addEventListener("DOMContentLoaded", function() {
     function syncUpdateCheck() {
 		checkUpdateStatus();
-		
+
         const now = new Date();
         const delay = 60000 - (now.getSeconds() * 1000 + now.getMilliseconds());
 
